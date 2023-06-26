ccececec94466bb2eb526b049210fef5a26bb3d7
SISTEMAS8\SYSTEM8 <lquispe@tecnologiatextil.com>
2023-06-26 07:53:04 -0500

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginasRoutingModule } from './paginas/paginas-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',redirectTo:'/login',pathMatch:'full'}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+    PaginasRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { PaginasModule } from './paginas/paginas.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    PaginasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
